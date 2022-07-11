de31ded32daf9dea6fa758b98d18a1aedc617732
tommyAyres <pmavoider@yahoo.com>
2022-07-11 13:12:56 -0500

    added react strap

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import 'font-awesome/css/font-awesome.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
 
+
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 
