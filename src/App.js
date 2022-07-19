f9414d08e2a689154710e0bda19f314d7f480108
tommyAyres <pmavoider@yahoo.com>
2022-07-19 17:06:07 -0500

    router setup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import './App.css';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import {Routes, Route} from"react-router-dom"
+import ContactPage from "./pages/ContactPage"
+import HomePage from "./pages/HomePage"
+
 
 
 function App() {
   return (
     <div className='App'>
    <Header />
-    <CampsitesDirectoryPage  />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="contact" element={<ContactPage />} />
+      <Route path="directory" element={<CampsitesDirectoryPage />} />
+    </Routes>
     <Footer />
 </div>
   );
